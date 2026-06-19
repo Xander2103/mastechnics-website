121cafb2a9cbc374e8543639b6bf03bb4c10d29a
Xander <xandervanmalder@gmail.com>
2026-06-19 13:25:18 +0200

    feat(site): add reviews config with placeholder testimonials

---
+++ config/reviews.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'enabled' => true,
+
+    'google_review_url' => 'https://www.google.com/search?sca_esv=ec2bff8bd1e2ef21&cs=0&sxsrf=ANbL-n6DQwM8rkU6sXsHhz5rWC_EznkXXQ:1781595564528&q=Mas+Technics+Reviews&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxI2MjQyMzA3MjAxNLU0N7IwNTEw3MDI-IpRxDexWCEkNTkjLzO5WCEotSwztbx4EStWYQA4mFqKSAAAAA&rldimm=2126072041597285401&tbm=lcl&hl=nl-BE&sa=X&ved=2ahUKEwiYiYbWoIuVAxW6V6QEHTAlO8YQ9fQKegQIEhAG&biw=2276&bih=1197&dpr=1.13#lkt=LocalPoiReviews',
+
+    'reviews' => [
+        [
+            'name'     => 'Thomas V.',
+            'location' => 'Antwerpen',
+            'rating'   => 5,
+            'text'     => 'Snel geholpen na mijn CV-ketelstoring. De technici waren punctueel, netjes en duidelijk in hun uitleg. Aanrader.',
+            'service'  => null,
+            'date'     => '2025-10',
+        ],
+        [
+            'name'     => 'Nathalie D.',
+            'location' => 'Gent',
+            'rating'   => 5,
+            'text'     => 'Airco laten installeren voor twee slaapkamers. Vlotte communicatie vooraf, een correcte offerte en een nette afwerking. Zeer tevreden.',
+            'service'  => null,
+            'date'     => '2025-09',
+        ],
+        [
+            'name'     => 'Marc L.',
+            'location' => 'Bruxelles',
+            'rating'   => 5,
+            'text'     => 'Service rapide et professionnel pour la maintenance de ma chaudière. Bon contact, travail soigné. Je recommande sans hésiter.',
+            'service'  => null,
+            'date'     => '2025-11',
+        ],
+        [
+            'name'     => 'Lieselotte B.',
+            'location' => 'Leuven',
+            'rating'   => 5,
+            'text'     => 'Waterverzachter geïnstalleerd met duidelijke uitleg over gebruik en onderhoud. Precies wat we nodig hadden, vlot en proper uitgevoerd.',
+            'service'  => null,
+            'date'     => '2025-08',
+        ],
+    ],
+];
